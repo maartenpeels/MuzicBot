02e03daeda77176fdc35cc922185b950bc4d01d7
Maarten Peels <maartenpeels@gmail.com>
2024-11-27 22:40:49 +0100

    Hook up skip command

--- bot/cmd/root.go
+++ bot/cmd/root.go
@@ -23,6 +23,10 @@ var Commands = []*discordgo.ApplicationCommand{
 		},
 	},
 	{
+		Name:		 "skip",
+		Description: "Skip the current song"
+	}
+	{
 		Name:        "stop",
 		Description: "Stop the music",
 	},
@@ -31,5 +35,6 @@ var Commands = []*discordgo.ApplicationCommand{
 var CommandHandlers = map[string]func(ctx *core.Context){
 	"ping": PingCommandHandler,
 	"play": PlayCommandHandler,
+	"skip": SkipCommandHandler,
 	"stop": StopCommandHandler,
 }
